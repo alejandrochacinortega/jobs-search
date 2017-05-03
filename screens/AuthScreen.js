a244648eeac94ecc127f37d965b8c5175ef94f29
Omar Alejandro Chacin Ortega <omaralejandrochacinortega@Omars-MacBook-Pro.local>
2017-05-03 12:44:59 +0200

    button style

--- screens/AuthScreen.js
+++ screens/AuthScreen.js
@@ -2,10 +2,6 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 class AuthScreen extends Component {
-  // static navigationOptions = {
-  //   tabBarLabel: 'kkkk',
-  // };
-  
   
   render() {
     return (
@@ -22,49 +18,3 @@ class AuthScreen extends Component {
 }
 
 export default AuthScreen;
-
-
-// import Expo from 'expo';
-// import React from 'react';
-// import { StyleSheet, Text, View, Button } from 'react-native';
-// import WelcomeScreen from './screens/WelcomeScreen';
-// import AuthScreen from './screens/AuthScreen';
-// import { TabNavigator } from 'react-navigation';
-//
-//
-// class MyHomeScreen extends React.Component {
-//   static navigationOptions = {
-//     tabBarLabel: 'Luis',
-//   };
-//
-//   render() {
-//     return (
-//       <Text>HHhHH</Text>
-//     );
-//   }
-// }
-//
-// class MyNotificationsScreen extends React.Component {
-//   static navigationOptions = {
-//     tabBarLabel: 'kkkk',
-//   };
-//
-//   render() {
-//     return (
-//       <Text>HHhHH</Text>
-//     );
-//   }
-// }
-//
-//
-// const MyApp = TabNavigator({
-//   Home: {
-//     screen: MyHomeScreen,
-//   },
-//   Notifications: {
-//     screen: MyNotificationsScreen,
-//   },
-// });
-//
-//
-// Expo.registerRootComponent(MyApp);
