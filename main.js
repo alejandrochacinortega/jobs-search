14da2b0a34a93cd7b8fd152bade1cfef331dcd1e
Omar Alejandro Chacin Ortega <omaralejandrochacinortega@Omars-MacBook-Pro.local>
2017-05-03 12:08:01 +0200

    add stack navigator

--- main.js
+++ main.js
@@ -3,14 +3,31 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import WelcomeScreen from './screens/WelcomeScreen';
 import AuthScreen from './screens/AuthScreen';
-import { TabNavigator } from 'react-navigation';
+import MapScreen from './screens/MapScreen';
+import DeckScreen from './screens/DeckScreen';
+import ReviewScreen from './screens/ReviewScreen';
+import SettingScreen from './screens/Setting';
+
+import { TabNavigator, StackNavigator } from 'react-navigation';
 
 class App extends React.Component {
   render() {
     
     const MainNavigator = TabNavigator({
       welcome: { screen: WelcomeScreen },
-      auth: { screen: AuthScreen }
+      auth: { screen: AuthScreen },
+      main: {
+        screen: TabNavigator({
+          map: { screen: MapScreen },
+          deck: { screen: DeckScreen },
+          review: {
+            screen: StackNavigator({
+              review: { screen: ReviewScreen },
+              settings: { screen: SettingScreen }
+            })
+          }
+        })
+      }
     });
     
     return (
@@ -27,8 +44,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
-
-
 
 
 Expo.registerRootComponent(App);
--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Button } from 'react-native-elements';
 
 class ReviewScreen extends Component {
+  static navigationOptions = {
+    title: 'Review Jobs',
+  };
+  
   render() {
     return (
       <View>
